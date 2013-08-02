7e5fa87f8fa8e6084f19ecf8011dfe2425a3c704
J.L. Canales Gasco <jlcanales.gasco@gmail.com>
2013-08-02 13:44:42 +0200

    [StmtSubscriber] Add Statement object to support Esper subscribers attached to EPL

---
+++ rotary-core/src/test/java/org/rotarysource/core/statements/SubscriberMock.java
@@ -0,0 +1,22 @@
+package org.rotarysource.core.statements;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Non abstract class used to be Mocked by
+ * Mockito
+ * @author J. L. Canales
+ *
+ */
+public class SubscriberMock {
+
+	private static Logger log = LoggerFactory.getLogger(SubscriberMock.class);
+	
+	public void update(Integer event) {
+		// This method is intenctionaly Empty.
+		// It will be Mocked by Mockito
+		log.info("Received Integer Event : {}",event);
+	}
+
+}
