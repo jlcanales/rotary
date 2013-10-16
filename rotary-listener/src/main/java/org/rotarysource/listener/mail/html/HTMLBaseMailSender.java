480b2b6aaeae9e140b07fd0147082f4d6615080b
J.L. Canales Gasco <jlcanales.gasco@gmail.com>
2013-10-16 11:50:00 +0200

    [MailListener] Velocity HTML mail sender implemented.

---
+++ rotary-listener/src/main/java/org/rotarysource/listener/mail/html/HTMLBaseMailSender.java
@@ -0,0 +1,182 @@
+package org.rotarysource.listener.mail.html;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.rotarysource.listener.mail.OAMailSender;
+import org.rotarysource.listener.mail.domain.MailDataBean;
+import org.rotarysource.signals.AppContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+public class HTMLBaseMailSender implements OAMailSender {
+
+	private static Log logger = LogFactory.getLog(HTMLBaseMailSender.class);
+
+	/**
+	 * Mail Sender needed to connect with SMTP server
+	 */
+	private JavaMailSender   mailSender;
+	
+	/**
+	 * Mail text charset
+	 */
+	private Charset			 charset;
+	
+	/**
+	 * Create a new ProofingHTMLMailSendSMTPImpl object
+	 */
+	public HTMLBaseMailSender(){
+		charset = Charset.forName("UTF-8");
+		
+	}
+	
+	@Override
+	public void sendMail(MailDataBean setupParams, String mailText,
+			String[] attachments, String[] resources) throws IllegalArgumentException,
+			MailException, RuntimeException {
+		//Input data validation
+		if( setupParams.getFromAddress().isEmpty()   ||
+			setupParams.getFromName().isEmpty() ||
+			setupParams.getSubject().isEmpty()  ||
+			setupParams.getTo().isEmpty()){
+			
+			String error ="setup Params not correctly defined. Empty fields detected";
+            logger.error(error);
+			throw new IllegalArgumentException(error);
+		}
+
+		// create a mime message using the mail sender implementation
+		MimeMessage message = mailSender.createMimeMessage();
+		// create the message using the specified template
+		MimeMessageHelper helper;
+        try {
+        	helper = new MimeMessageHelper(message, true, this.charset.name());
+			helper.setFrom(setupParams.getFromAddress(), setupParams.getFromName());
+	        helper.addTo(setupParams.getTo());
+			
+			message.setSubject(setupParams.getSubject()); 
+			helper.setSubject(setupParams.getSubject());
+			
+			helper.setText(mailText,true);
+			
+			if (logger.isDebugEnabled())
+				logger.debug("Envio de email " + mailText);
+			
+			if (attachments!=null) {
+				for (String attach: attachments ){
+					if (attach != null) {
+						addAttachments( helper, attach);
+					}
+				}
+			}
+
+			if (resources != null) {
+				for (String resource: resources ){
+					if (resource != null) {
+						addInlineResource( helper, resource);
+					}
+				}
+			}
+			
+			mailSender.send(helper.getMimeMessage());
+
+
+        } catch (UnsupportedEncodingException e) {
+ 			String error="Unable to setup the mime message helper (setFrom, addTo) for: ";
+ 			error += setupParams.getTo() + " Email Address." + "\n";
+ 			if( logger.isDebugEnabled()) error += e.getMessage();
+ 			
+ 			logger.error(error);
+ 			throw new RuntimeException(error);
+        } catch (MessagingException e) {
+			String error="Unable to setup the message template for: ";
+			error += setupParams.getTo() + " Email Address." + "\n";
+			if( logger.isDebugEnabled()) error += e.getMessage();
+			
+			logger.error(error);
+			throw new RuntimeException(error);
+		}catch(MailException e){
+			String error ="Error sending email for "  +
+					setupParams.getTo() + " Email Address." + "\n" + e.getMessage();
+            logger.error(error);
+			throw e;
+		}
+	}
+	
+    /**
+     * Add Attachments configured in this Item to the message.
+     * If there isnt any configured attachment or a file access expeption
+     * happens, no attachment will be added
+     * @param helper 
+     *		MimeMessageHelper to manage the mail message.
+     * @param resource 
+     *		File name to attach
+     */
+    private void addAttachments( MimeMessageHelper helper, String attachment){
+    	 	
+        	ApplicationContext  ctx 	   = AppContext.getApplicationContext(); 
+            Resource 			fileAttach = ctx.getResource(attachment);
+
+            try {
+    			FileSystemResource file = new FileSystemResource(fileAttach.getFile());
+    			helper.addAttachment(file.getFilename(), file);
+    		} catch (Exception e1) {
+    			String error="Unable to attach file to the Email: ";
+    			error += e1.getMessage();
+    			logger.warn(error);
+    		} 
+    }
+
+
+    /**
+     * Add inline resource configured in this Item to the message.
+     * If there isnt any configured inline element or a file access exception
+     * happens, no attachment will be added
+     * @param helper 
+     *		MimeMessageHelper to manage the mail message.
+     * @param resource 
+     *		File name to attach
+     */
+    private void addInlineResource( MimeMessageHelper helper, String resource){
+    	 	
+        	ApplicationContext  ctx 	   = AppContext.getApplicationContext(); 
+            Resource 			fileAttach = ctx.getResource(resource);
+
+            try {
+    			FileSystemResource file = new FileSystemResource(fileAttach.getFile());
+    			helper.addInline(file.getFilename(), file);
+    		} catch (Exception e1) {
+    			String error="Unable to embed file to the Email: ";
+    			error += e1.getMessage();
+    			logger.warn(error);
+    		} 
+    }
+    
+	/**
+	* Return the JavaMailSender used to send emails
+	* @return JavaMailSender Mail Sender.
+	*/		
+	public JavaMailSender getMailSender() {
+		return mailSender;
+	}
+
+	/**
+	* Set the JavaMailSender used to send emails. Mail sender must be setted up
+	* @param aiMailSender Java Mail Sender.
+	*/
+	public void setMailSender(JavaMailSender aiMailSender) {
+		this.mailSender = aiMailSender;
+	}
+
+}
---
+++ rotary-listener/src/test/java/org/rotarysource/listener/mail/VelocityMailSenderTest.java
@@ -0,0 +1,95 @@
+package org.rotarysource.listener.mail;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+import javax.annotation.Resource;
+import javax.mail.internet.MimeMessage;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.rotarysource.listener.mail.domain.MailDataBean;
+import org.rotarysource.listener.mail.html.VelocityHTMLMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import junit.framework.TestCase;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration(locations = { "classpath:MailListenerTest-config.xml" })
+public class VelocityMailSenderTest extends TestCase {
+
+	/**
+	 * Cep Engine to support listener execution
+	 */
+	@Resource
+	private VelocityHTMLMailSender velocityHTMLMailSender;
+
+	@Resource 
+	private JavaMailSenderImpl javaMailSenderMock;
+	
+	@Mock
+	MimeMessage mimeMessageMock;
+	
+	private MailDataBean mailData;
+
+
+	/**
+	 * Initialize Mocks in order to create a Mock structure that allow
+	 * contractStubMock return correct Axis2 interface objects to he 
+	 * Object Under Test
+	 */
+	@Before
+	public void setUp(){
+		MockitoAnnotations.initMocks( this );
+			
+			// Prepare javaMailSenderMock to return a mock object representing
+			// MimeMessage
+			when(javaMailSenderMock.createMimeMessage()).thenReturn(mimeMessageMock);
+		
+				
+		
+		// Mail Data inicialization
+		mailData = new MailDataBean();
+		
+		mailData.setFromAddress("mmm@test.com");
+		mailData.setFromName("Test origin");
+		mailData.setSubject("HTML test ");
+		mailData.setTo("aaa@test.com");
+	}
+	
+	/**
+	 * Test AppContext Loading to assure all needed beans
+	 * are properly configured.
+	 */
+	@Test
+	public void loadAppContextTest(){
+		Assert.assertTrue(true);
+	}
+
+	/**
+	 * Basic smoke test for send method.
+	 */
+	@Test
+	public void sendSmokeTest(){
+		
+		HashMap<String, Object> composeParams = new HashMap<String, Object>();
+		composeParams.put("param1", "param1 value");
+		
+		velocityHTMLMailSender.sendMail(mailData, composeParams, null, null);
+		
+	}
+
+}
